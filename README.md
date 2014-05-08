91c8a8d1ba449eb8b23fbeffa5386018c78090b0
Luke Hudson <lukeletters@gmail.com>
2014-05-08 18:15:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Huayra stopmotion gphoto2 client #
+
+This is a work in progress to allow [Huayra Stopmotion](https://github.com/HuayraLinux/huayra-stopmotion)
+to use the ghoto2 backend to provde cameras.
+
+Currently huayra-stopmotion listens via WebSockets for cameras, and provides a webpage to easily connect your webcam or mobile.
+
+I wanted a way to connect my Canon EOS as a provider of images.
+
+This script is the inital implementation.
+
+## Usage ##
+
+Load huayra-stopmotion and create a new project or load an existing one.
+In the 'Opciones' tab you'll see the address where huayra is listening for cameras.
+
+Run the following:
+`node gphotoclient.js <PORT>`
+where *PORT* is the port on which huayra is listening.
+
+
---
+++ gphotoclient.js
@@ -0,0 +1,147 @@
+var spawn = require('child_process').spawn;
+var socket = require("socket.io-client");
+var tmp = require('tmp');
+var path = require('path');
+var fs = require('fs');
+
+if (process.argv.length < 3) {
+	console.error("Please provide port # as arg");
+	process.exit(1);
+}
+var port = process.argv[2];
+
+
+/**
+ * Simulate image capture using a static file
+ */
+function captureImageSim(conn) {
+	emitImage(conn, 'capture.jpg');
+}
+
+/**
+ * Emit image data
+ */
+function emitImage(conn, filename) {
+	try {
+		var data = fs.readFileSync(filename);
+		data = 'data:image/jpeg;base64,' + data.toString('base64');
+		conn.emit('captura', {data: data});
+	} catch(ex) {
+		console.error(ex);
+	}
+}
+
+
+/**
+ * Configure camera
+ * @todo: This needs to use an external config file to allow for easy
+ *  configuration for specific cameras etc.
+ */
+function configureCamera(callback) {
+	var config = {
+		imageformat: 5,
+		capturetarget: 0,
+		capture: 'on'
+	};
+	var args = [];
+	for(c in config) {
+		if (config.hasOwnProperty(c)) {
+			args.push('--set-config');
+			args.push(c + '=' + config[c]);
+		}
+	}
+	var proc = spawn('gphoto2', args);
+	var errors = null;
+	proc.stderr.on('data', function (data) {
+		//console.error('configureCamera', data.toString());
+		errors = data.toString();
+	});
+	proc.on('exit', function(code) {
+		if (typeof callback === 'function') {
+			callback(errors, code);
+		}
+	});
+}
+
+
+/**
+ * Use gphoto2 to capture images and emit the data via websockets
+ * @todo: TIDY UP a LOT and add more appropriate error handling
+ */
+function captureImage(conn) {
+	tmp.tmpName(function _tmpnameGenerated(err, tempPath) {
+		// Regexes to match gphoto output
+		var rxError = /Error/gi;
+		var rxSaving = /Saving file as ([^.jpg]+)/g;
+		var rxSaved = /New file is in/g;
+
+		var saved = null; // saved file path, extracted from gPhoto output
+
+		console.log('Spawn gphoto2', args);
+		var dir = path.dirname(tempPath);
+		var args = [ "--capture-image-and-download", "--force-overwrite", "--filename=" + tempPath];
+		var capture = spawn("gphoto2", args, { cwd: dir });
+
+		// Listen for errors
+		capture.stderr.on("data", function _stderrData(data) {
+			console.error("\tError with gphoto", data.toString());
+		});
+		// Listen for output from gphoto
+		capture.stdout.on("data", function _stdoutData(data) {
+			console.log("gphoto2 >>> ");
+			// code here was adapted from https://github.com/porkbuns/shmile
+			dataStr = data.toString();
+			if (dataStr.match(rxError)) {
+				console.error('\tError with gphoto', data.toString());
+			} else {
+				if (dataStr.match(rxSaved)) {
+					console.log("\tfinished saving image.");
+					emitImage(conn, saved);
+				}
+				saved = rxSaving.exec(dataStr);
+				if (saved && saved.length === 2) {
+					saved = saved[1] + '.jpg';
+					console.log('\tsaving image as ', saved);
+				} else {
+					console.warn("\tUnknown response from gphoto2", dataStr);
+				}
+			}
+		});
+	});
+}
+
+/**
+ * Start the websockets client
+ */
+function startClient(captureFn) {
+	var conn = socket.connect('http://localhost:' + port);
+	console.log(conn);
+
+	conn.on('connect', function() {
+		console.log("Connected.");
+		setInterval(function _timerFn() { captureFn(conn); }, 1000);
+		conn.on('capturaSuccess', function(data) {
+			console.log('server responded:', data);
+		});
+		conn.on('disconnect', function() {
+			console.log("Disconnected.", conn);
+			process.exit(0);
+		});
+	});
+	console.log('end startClient');
+}
+
+
+/**
+ * Try to configure camera
+ * In case of error, for now fallback to simulation (for debugging)
+ */
+configureCamera(function(err, exitCode) {
+	if (err || exitCode !== 0) {
+		console.error('Failed to configure camera.  GPhoto2 says: ', err);
+		startClient(captureImageSim);
+	} else {
+		startClient(captureImage);
+	}
+});
+
